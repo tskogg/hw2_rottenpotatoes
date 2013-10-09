35e0e62216fab34c439b62772df44287392b66e5
Tayler Skogg <tayler.skogg@colorado.edu>
2013-10-09 09:32:17 -0700

    Enhancements phase 1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,26 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    sort = params[:sort] || session[:sort]
+    case sort
+    when 'title'
+		ordering,@title_header ={:order => :title}, 'hilite'
+	when 'release_date'
+		ordering,@date_header = {:order => :release_date}, 'hilite'
+	end
+	@all_ratings = Movie.all_ratings
+	@selected_ratings = params[:ratings] || session[:ratings] || {}
+	
+	if @selected_ratings == {}
+		@selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}]
+	end
+	
+	if params[:sort] != session[:sort] or params[:ratings] != session[:ratings]
+		session[:sort] = sort
+		session[:ratings] = @selected_ratings
+		redirect_to :sort =? sort, :ratings => @selected_ratings and return
+	end
+	@movies = Movie.find_all_by_rating(@selected_ratings.keys, ordering)
   end
 
   def new
